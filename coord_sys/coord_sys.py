2f3e370431319575968f375342e48034765f0f9e
freude <misha.klymenko@gmail.com>
2015-08-11 16:58:47 +0200

    myconst

--- coord_sys/coord_sys.py
+++ coord_sys/coord_sys.py
@@ -24,3 +24,36 @@ class CoordSys(object):
 
     #----------------------------------------------------------------------
 
+    def set_origin_cells(self, origin_cells):
+            self.origin_cells = origin_cells;
+            self.origin_inds=self.arrays_sizes*(self.origin_cells-1)+1;
+            self.coord_limits(1)=-self.origin_inds(1)*self.coord_stps+self.coord_stps;
+            self.coord_limits(2)=-self.origin_inds(1)*self.coord_stps+self.num_cells*self.lattice_const;
+        end
+
+        [kx ,ky ,kz] = global_basis_rec(obj,j1,j2,j3)
+        [x ,y ,z] = global_basis(obj,j1,j2,j3)
+
+        function [x ,y ,z] = global_basis_array(obj,X,Y,Z)
+            [x ,y ,z] = arrayfun(@obj.global_basis,X,Y,Z);
+        end;
+
+        function [x ,y ,z] = global_basis_rec_array(obj,X,Y,Z)
+            [x ,y ,z] = arrayfun(@obj.global_basis_rec,X,Y,Z);
+        end;
+
+        function [x ,y ,z] = global_coords_gen(obj)
+            x=1:obj.coord_sizes;
+            [X,Y,Z]=meshgrid(x,x,x);
+            [x ,y ,z] = arrayfun(@obj.global_basis,X,Y,Z);
+        end;
+
+        function [x ,y ,z] = global_coords_rec_gen(obj)
+            x=1:obj.coord_sizes;
+            [X,Y,Z]=meshgrid(x,x,x);
+            [x ,y ,z] = arrayfun(@obj.global_basis_rec,X,Y,Z);
+        end;
+
+        function x = x(obj)
+            x = obj.coord_limits(1):obj.coord_stps:obj.coord_limits(2);
+        end;
